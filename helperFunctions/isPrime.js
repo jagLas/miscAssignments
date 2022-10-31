5a7309a9f2b77334af8c288f9d2359311662b024
jagLas <jglasher@gmail.com>
2022-10-31 15:21:55 -0400

    started helper functions

---
+++ helperFunctions/isPrime.js
@@ -0,0 +1,18 @@
+let isPrime = function(num) {
+    if (num < 2){
+        return false;
+    }
+    for (let i = 2; i < num; i++){
+        if (num % i === 0) {
+            return false;
+        };
+    };
+    return true;
+};
+
+console.log(isPrime(2)); // => true
+console.log(isPrime(10)); // => false
+console.log(isPrime(11)); // => true
+console.log(isPrime(9)); // => false
+console.log(isPrime(2017)); // => true
+console.log(isPrime(1)); // => false
