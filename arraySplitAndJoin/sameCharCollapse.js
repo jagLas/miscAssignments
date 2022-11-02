f3177a2edae8050139e84d7767c0bd64e5caa2ce
jagLas <jglasher@gmail.com>
2022-11-01 23:18:01 -0400

    end of Array Split and Join

--- arraySplitAndJoin/sameCharCollapse.js
+++ arraySplitAndJoin/sameCharCollapse.js
@@ -1,4 +1,19 @@
 function sameCharCollapse(str) {
+    let noPairFound = false;
+
+    while (noPairFound ===false) {
+        if (deletePair(str) === 'no pairs'){
+            noPairFound = true;
+        }   else {
+            str = deletePair(str);
+        }
+
+    }
+    return str;
+
+}
+
+function deletePair(str) {
     let chars = str.split('');
 
     for (let i = 0; i < chars.length; i++) {
@@ -7,12 +22,11 @@ function sameCharCollapse(str) {
 
         if (char === nextChar){
             chars.splice(i, 2);
+            return chars.join('');
         }
     }
-    return chars
+    return 'no pairs';
 }
-
-//REPEAT MORE TIMES
 
 console.log(sameCharCollapse("zzzxaaxy"));  // "zy"
 // because zzzxaaxy -> zxaaxy -> zxxy -> zy
