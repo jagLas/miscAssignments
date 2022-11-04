638103df11a822147364d634ce245d69ebca94ea
jagLas <jglasher@gmail.com>
2022-11-04 16:04:43 -0400

    finished destructure practice

---
+++ destructuring/destructureArray.js
@@ -0,0 +1,15 @@
+let bigArray = ["apple", 14, 32, 100, { name: "party" }, ["pineapple"]];
+
+let [firstEl, secondEl, thirdEl, fourthEl, fifthEl, sixthEl] = bigArray;
+
+console.log(firstEl);
+console.log(secondEl);
+console.log(thirdEl);
+console.log(fourthEl);
+console.log(fifthEl);
+console.log(sixthEl);
+
+// Example:
+// let [firstEl] = bigArray;
+// console.log(firstEl) // prints "apple"
+
